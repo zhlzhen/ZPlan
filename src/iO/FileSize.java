9460f1bb97a3aef7139cf6927e6e3204dc22ac71
zh <zhlzhai@126.com>
2014-11-03 23:44:05 +0800

    文件的大小的计算

---
+++ src/iO/FileSize.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package iO;
+
+/**
+ * @author zha
+ *
+ */
+public class FileSize {
+
+	/**
+	 * 
+	 */
+	public FileSize() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
